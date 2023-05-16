bc3cf408b968f48a9a1938d85bb16a0bd2cdab74
Wee Ching Hwa <weechinghwa@outlook.com>
2023-05-16 20:22:02 +0800

    pair_clusters

--- Compute/calc_config.py
+++ Compute/calc_config.py
@@ -45,7 +45,7 @@ def HFground_pair_list(num_particles:"tuple",num_orbitals:"tuple")->"list":
     nucl_state_init = neut_state_init + prot_state_init
     pair_list = list(combinations(nucl_state_init,2))
     return pair_list
-def custom_excitation_list(num_spatial_orbitals: int,num_particles:tuple): ## note: num_orbitals is crutial information, but it is not input(parameter_py takes care of this)
+def like_qiskit(num_spatial_orbitals: int,num_particles:tuple): ## note: num_orbitals is crutial information, but it is not input(parameter_py takes care of this)
     # the function is of resemblance of the excitations selected in the Hamiltonian, but not really"
     non_repeat_list = list(combinations(range(0,sum(num_orbitals)),2))
     
@@ -75,7 +75,39 @@ def custom_excitation_list(num_spatial_orbitals: int,num_particles:tuple): ## no
     my_excitation_list.sort()
     return my_excitation_list
 
-vqe_excitations = custom_excitation_list # or replace with "d" if wish to use default vqe_excitations list
+def pair_clusters(num_spatial_orbitals: int,num_particles:tuple): ## note: num_orbitals is crutial information, but it is not input(parameter_py takes care of this)
+    # the function is of resemblance of the excitations selected in the Hamiltonian, but not really"
+    non_repeat_list = list(combinations(range(0,sum(num_orbitals)),2))
+    
+    neut_orbitals_list = list(range(0,num_orbitals[0]))
+    prot_orbitals_list = list(range(num_orbitals[0], sum(num_orbitals)))
+    neut_state_list = list(range(0,num_particles[0]))
+    prot_state_list = list(range(num_orbitals[0], num_orbitals[0] + num_particles[1]))
+
+    init_state_indeces = neut_state_list + prot_state_list
+    init_pair_list = HFground_pair_list(num_particles, num_orbitals) ## same functioned used in the txt_read.ipynb
+    number_pairs = [(i, i+1) for i in range(0, 20, 2)]
+
+    allowed_init = list(set(non_repeat_list) & set(init_pair_list) & set(number_pairs))
+    allowed_fina = non_repeat_list
+    my_excitation_list = []
+
+    for init in allowed_init:
+        for fina in allowed_fina:
+            if (fina[0] in init_state_indeces) or (fina[1] in init_state_indeces):
+                pass
+            elif (((init[0] in neut_orbitals_list) == (fina[0] in neut_orbitals_list) ==
+                   (init[1] in neut_orbitals_list) == (fina[1] in neut_orbitals_list)) and
+                  ((init[0] in prot_orbitals_list) == (fina[0] in prot_orbitals_list) ==
+                   (init[1] in prot_orbitals_list) == (fina[1] in prot_orbitals_list)) and
+                  (init in allowed_init)
+                 ):
+                excitations = [init,fina];
+                my_excitation_list.append(tuple(excitations) if (excitations not in my_excitation_list) else tuple())
+    my_excitation_list.sort()
+    return my_excitation_list
+
+vqe_excitations = pair_clusters # or replace with "d" if wish to use default vqe_excitations list
 
 
 
