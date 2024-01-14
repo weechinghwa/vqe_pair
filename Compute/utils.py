2f0b737d86449b90a4781c8e6eccf4f363f0d71c
Wee Ching Hwa <weechinghwa@outlook.com>
2024-01-14 22:06:04 +0800

    ln fit 10 step

--- Compute/utils.py
+++ Compute/utils.py
@@ -277,6 +277,27 @@ class TerminateLnFit:
                 return True
         return False
 
+class TerminateLnFit10step:
+ 
+    def __init__(self, N: int, coeff : float):
+        self.N = N #number of steps before accepting trigger
+        self.coeff = coeff
+        self.values = []
+        self.collected = []
+ 
+    def __call__(self, nfev, parameters, value, stepsize, accepted) -> bool:
+        self.values.append(value)
+        self.collected.append((nfev, parameters, value, stepsize, accepted))
+        if (len(self.values) > self.N and 
+            len(self.values) % 10 == 0):
+            last_values = np.array(self.values)
+            x_array = np.arange(1, len(self.values) + 1)
+            ln_fit = np.polyfit(np.log(x_array), last_values, 1)
+            coef_log = ln_fit[0] 
+
+            if 0 > coef_log > self.coeff:
+                return True
+        return False
 ## ORIginal excitations function
 # def custom_excitation_list(num_spatial_orbitals: int,
 #                            num_particles: tuple[int, int]):
--- Compute/vqe.py
+++ Compute/vqe.py
@@ -1,5 +1,5 @@
 from calc_config import *
-from utils import TerminateThreeStep, TerminatePovSlope, TerminateThreeSMA, TerminateMinSlope, TerminateLnFit
+from utils import TerminateThreeStep, TerminatePovSlope, TerminateThreeSMA, TerminateMinSlope, TerminateLnFit, TerminateLnFit10step
 
 ## Define callback function: to collect convergence information
 counts = []; values = []; stdeviation = []; param_list = []
@@ -72,7 +72,8 @@ if optmz =="SPSA":
                "TerminateThreeSMA": TerminateThreeSMA(N = 10, tol = optimizer_tol),
                "TerminateMinSlope": TerminateMinSlope(N = 10),
                "TerminateLnFit_neg_0.112" : TerminateLnFit(N = 10, coeff=-0.112),
-               "TerminateLnFit_neg_0.1" : TerminateLnFit(N = 10, coeff=-0.1)
+               "TerminateLnFit_neg_0.1" : TerminateLnFit(N = 10, coeff=-0.1),
+               "TerminateLnFit10step_neg_0.1" : TerminateLnFit10step(N=10, coeff=-0.1)
                }
     termination_checker = tc_dict[tc]
     optimizer = SPSA(maxiter=optimizer_maxiter,termination_checker=termination_checker, callback=SPSA_callback)
